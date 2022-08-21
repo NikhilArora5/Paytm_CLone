87c4ca02ab24d836ef39c204d5a9ca276b3bdc4a
Nikhil Arora <aroranikhil8184@gmail.com>
2022-08-21 23:29:40 +0530

    arrow anim rem & mid card full resp pend

---
+++ script.js
@@ -0,0 +1,30 @@
+
+// document.getElementById("menu").addEventListener("click",onClickMenu,false);
+
+
+// collapse nav script
+function onClickMenu(){
+  
+
+    document.getElementById("cp-menu").classList.toggle("icon");
+    document.getElementById("nav-cp").classList.toggle("change");
+}
+
+
+
+
+function arrowfunc(){
+
+    document.getElementById("arrow-1").classList.toggle("arrow-1-right");
+    // document.getElementById("arrow-2").classList.toggle("arrow-2-display");
+
+}
+
+
+function x(){
+
+    document.getElementById("arrow-1").classList.toggle("arrow-1-left");
+    // document.getElementById("arrow-2").classList.toggle("arrow-2-display");
+
+
+}
